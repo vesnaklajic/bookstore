8d623c351e06c49dd42121fd03e2a256ff425064
hagualshaik <hagualshekh@gmail.com>
2021-02-25 13:08:36 +0100

    create a controllers genres object

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,15 @@
 // controllers dépendencies
+const genres = require('../config/routes/genres');
 const services = require('../services/index');
 
 // controllers
 const user_controller = require('./user');
+genres_constroller = require('./genres')
 
 // create a controllers object for map all the controllers
 const controllers = {
-    user: user_controller(services)
+    user: user_controller(services),
+    genre: genres_constroller(services)
 }
 
 // export our controllers object
