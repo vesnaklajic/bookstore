2b0ffebc94383777e729ab93cebb3318086e74c1
Thomas Bousquet <tomasbousquet@gmail.com>
2021-02-25 19:19:44 +0100

    fix rebase conflict

--- services/index.js
+++ services/index.js
@@ -2,20 +2,24 @@
 const repositories = require('../repositories/index'),
     nodemailer = require("nodemailer"),
     bcrypt = require("bcrypt");
+    jwt = require('jsonwebtoken')
 
 // services
 const user_service = require('./user'),
-genres_service = require('./genres')
-    mailer_service = require('./mailer');
-    bcrypt_service = require('./bcrypt');
-    
+    genres_service = require('./genres'),
+    mailer_service = require('./mailer'),
+    bcrypt_service = require('./bcrypt'),
+    jwt_service = require('./jwt')
+    livre_service=require('./livre')
 
 // create a services object for map all the services
 const services = {
     genres: genres_service(repositories),
     user: user_service(repositories),
     mailer: mailer_service(nodemailer),
-    bcrypt: bcrypt_service(bcrypt)
+    bcrypt: bcrypt_service(bcrypt),
+    jwt: jwt_service(jwt),
+    livre: livre_service(livre)
 }
 
 // export our service object
