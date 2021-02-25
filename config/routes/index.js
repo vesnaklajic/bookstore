1bfcec322e82fb9e88167eb80d31dbec3dace05d
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-25 15:16:52 +0100

    .env surpres

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,6 +1,7 @@
 module.exports = (express, controllers) => {
     const user_route = require('./user');
     const cat_route=require('./cat');
+    
 
     const routes = [
         user_route(express, controllers),cat_route(express, controllers)
