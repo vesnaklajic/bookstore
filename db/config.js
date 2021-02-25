9a0a29e16f66f4bbc1a6cfee57b50b396438d55d
Thomas Bousquet <tomasbousquet@gmail.com>
2021-02-25 11:23:48 +0100

    Passage a create pool pour améliorer la stabilité de la connexion BDD

--- db/config.js
+++ db/config.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2'),
     config = require('../config/env');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: config.db_host,
     user: config.db_user,
     password: config.db_password,
-    database: config.db_name
+    database: config.db_name,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = db;
