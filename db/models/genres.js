7ace644aaedb6b7c76d673cb65a25695931c7214
hagualshaik <hagualshekh@gmail.com>
2021-02-25 13:16:17 +0100

    genres  intreface

---
+++ db/models/genres.js
@@ -0,0 +1,12 @@
+class genres {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async query(query, genres = []) {
+    const [rows, fields] = await this.db.promise().execute(query, genres);
+    return rows;
+  }
+}
+
+module.exports = genres;
