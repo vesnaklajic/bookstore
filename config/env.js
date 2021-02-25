7a0c2be34257d9391862b9a3f22edfcab7aeedd7
Thomas Bousquet <tomasbousquet@gmail.com>
2021-02-25 01:59:04 +0100

    Correction de bug configuration de la BDD + ajout d'un gitignore

--- config/env.js
+++ config/env.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
     app_port: process.env.APP_PORT,
     db_port: process.env.DB_PORT,
+    db_host: process.env.DB_HOST,
     db_name: process.env.DB_NAME,
     db_user: process.env.DB_USER,
     db_password: process.env.DB_PASSWORD,
