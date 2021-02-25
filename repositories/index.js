ca212dd9b45fb78506f712202fd0815c8e074331
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-25 17:19:38 +0100

    Livre added

--- repositories/index.js
+++ repositories/index.js
@@ -3,10 +3,14 @@ const models = require('../db/models/index')
 
 // Repositories
 const user_repository = require('./user');
+const cat_repository=require('./category');
+const livre_repository=require('./livre');
 
 // create a repositories object for map all the repositories
 const repositories = {
-    user: user_repository(models)
+    user: user_repository(models),
+    cat: cat_repository(models),
+    livre: livre_repository(models)
 }
 
 // export our repositories object
