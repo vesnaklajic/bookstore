9c5a207511aa64442eebcdb8c700d0adc3968b71
Thomas Bousquet <tomasbousquet@gmail.com>
2021-02-25 11:17:26 +0100

    Correction requette SQL par rapport à la nouvelle structure BDD

--- repositories/user.js
+++ repositories/user.js
@@ -1,14 +1,14 @@
 module.exports = (models) => {
     const user_repository = {
         getAll: async (data) => {
-            return models.user.query("select * from users");
+            return models.user.query("select * from Users");
         },
         register: async (data) => {
-            return models.user.query("INSERT INTO users (firstname, lastname, email, password) VALUES (?,?,?,?);",
+            return models.user.query("INSERT INTO Users (firstname, lastname, email, password) VALUES (?,?,?,?);",
              [data.firstname, data.lastname, data.email, data.password])
         },
         getById: async (id) => {
-            return models.user.query("select * from users where id = ? LIMIT 1", [id]);
+            return models.user.query("select * from Users where id = ? LIMIT 1", [id]);
         }
     }
     
